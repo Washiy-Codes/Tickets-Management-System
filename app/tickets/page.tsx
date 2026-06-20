3191aecd5813016081c30103ea2ea852db0a6326
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-20 16:09:25 +0300

    feat: implement tickets listing page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
 
-const TicketsPag = () => {
+import {Heading} from '@/components/heading';
+import Spinner from '@/components/spinner';
+import { TicketList } from '@/features/tickets/components/ticket-list';
+import { Suspense } from 'react';
+
+
+const TicketsPage = () => {
   return (
-    <div>
-        <h1>Tickets Page</h1>
+    <div className="flex flex-col gap-8">
+      <Heading title="Tickets Page" description="Your tickets page to view all tickets" />
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   )
 }
 
-export default TicketsPag
+export default TicketsPage
