1332a828a820697c59643ed265ac4c0001b658d6
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-20 16:09:11 +0300

    feat: update home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+import { Heading } from "@/components/heading";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Tickets App</h1>
-      <p>Manage your tickets efficiently and effortlessly.</p>
+    <div className="flex flex-col gap-8">
+      <Heading title="Homepage" description="Your homepage to start" />
+      <div className="flex flex-col flex-1 items-center justify-center gap-4">
+        <Link href={ticketsPath()} className="text-md underline">
+          Go to Tickets Page
+      </Link>
+      </div>
+      
     </div>
   );
 };
