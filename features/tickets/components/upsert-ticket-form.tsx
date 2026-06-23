9455b4ce2138a9ac56e56e1dfb1cc38155c671fb
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-24 02:39:46 +0300

    feat(forms): add deadline and bounty inputs to ticket form

--- features/tickets/components/upsert-ticket-form.tsx
+++ features/tickets/components/upsert-ticket-form.tsx
@@ -1,23 +1,45 @@
-import { Button } from "@/components/ui/button"
+"use client"
+import { useActionState } from "react"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import { Ticket } from "@/app/generated/prisma/client"
 import { upsertTicket } from "../actions/upsert-ticket"
+import { SubmitButton } from "@/components/form/submit-button"
+import { FieldErrors } from "@/components/form/field-errors"
+import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state"
+import { Form } from "@/components/form/form"
 type UpsertTicketFormProps = {
     ticket?: Ticket;
 }
 
+
 const UpsertTicketForm = ({ ticket }: UpsertTicketFormProps) => {
-     return (
-    <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-2">
+    const [actionState, action] = useActionState(upsertTicket.bind(null, ticket?.id), EMPTY_ACTION_STATE);
+    
+    return (
+    <Form action={action} actionState={actionState}>
       <Label htmlFor="title">Title</Label>
-      <Input type="text" id="title" name="title" defaultValue={ticket?.title} required />
+      <Input type="text" id="title" name="title" defaultValue={actionState.payload?.get("title") as string ?? ticket?.title} />
+      <FieldErrors actionState={actionState} name="title" />
       <Label htmlFor="content">Content</Label>
-      <Textarea id="content" name="content" defaultValue={ticket?.content} required></Textarea>
-      <Button type="submit">{ticket ? "Edit" : "Create"} Ticket</Button>
-    </form>
+      <Textarea id="content" name="content" defaultValue={(actionState.payload?.get("content") as string) ?? ticket?.content} ></Textarea>
+      <FieldErrors actionState={actionState} name="content" />
+      <div className="flex gap-x-2">
+        <div className="flex-1">
+         <label htmlFor="deadline">Deadline</label>
+         <Input type="datetime-local" id="deadline" name="deadline" defaultValue={(actionState.payload?.get("deadline") as string) ?? ticket?.deadline} />
+        </div>
+        <div className="flex-1">
+          <label>Bounty(USD)</label>
+          <Input type="number" id="bounty" name="bounty" defaultValue={actionState.payload?.get("bounty") as string ?? ticket?.bounty?.toString()} />
+        </div>
+      </div>
+      <SubmitButton label={ticket ? "Update" : "Create"} ticket={ticket} />
+    </Form>
   )
 }
 
-export {UpsertTicketForm}
+export { UpsertTicketForm }
+
+
