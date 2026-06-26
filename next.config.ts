d03d303cbfe2f8183bf5878b8b044b47eb0ec2eb
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-26 19:02:09 +0300

    chore: update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    experimental: {
+    clientRouterFilter: true,
     optimizePackageImports: [
       'lucide-react', 
       'react-icons',
