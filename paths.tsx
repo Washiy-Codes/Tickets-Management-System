76635bdee945f8ca01d178a70f4beeedc865c24c
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-26 19:01:53 +0300

    chore: update path utilities

--- paths.tsx
+++ paths.tsx
@@ -2,5 +2,8 @@ const ticketsPath = () => "/tickets";
 const ticketPath = (ticketId: string) => `/tickets/${ticketId}`;
 const homePath = () => "/";
 const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`;
+const signUpPath = () => "/sign-up";
+const signInPath = () => "/sign-in";
+const forgetPasswordPath = () => "/forget-password";
 
-export { ticketsPath, ticketPath, homePath, ticketEditPath };
+export { ticketsPath, ticketPath, homePath, ticketEditPath, signUpPath, signInPath, forgetPasswordPath };
