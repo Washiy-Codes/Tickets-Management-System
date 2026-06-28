6d253674168a232d18ecb6bf70345fd5c8717fc2
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-28 23:33:16 +0300

    chore(config): update project configuration and dependencies

--- paths.tsx
+++ paths.tsx
@@ -4,6 +4,6 @@ const homePath = () => "/";
 const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`;
 const signUpPath = () => "/sign-up";
 const signInPath = () => "/sign-in";
-const forgetPasswordPath = () => "/forget-password";
+const forgotPasswordPath = () => "/forgot-password";
 
-export { ticketsPath, ticketPath, homePath, ticketEditPath, signUpPath, signInPath, forgetPasswordPath };
+export { ticketsPath, ticketPath, homePath, ticketEditPath, signUpPath, signInPath, forgotPasswordPath };
