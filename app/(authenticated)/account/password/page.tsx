6d75f42fa3188f65b07abc9edaccbb4c39e0f0e4
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-07 22:48:15 +0300

    feat(account): update password management page

--- app/(authenticated)/account/password/page.tsx
+++ app/(authenticated)/account/password/page.tsx
@@ -1,11 +1,24 @@
 import { Heading } from "@/components/heading";
 import AccountTabs from "../_navigation/account-tabs";
+import { CardCompact } from "@/components/card-compact";
+import { PasswordChangeForm } from "@/features/password/components/password-change-form";
 
 const PasswordPage = () => {
     return (
-        <div>
-            <Heading title="Change Password" description="Update your password here." tabs={<AccountTabs />} />
-        </div>
+        <>
+        <Heading title="Change Password"
+            description="keep your account secure."
+            tabs={<AccountTabs />} />        
+        <div className="flex flex-1 flex-col gap-8 items-center justify-center">
+        <CardCompact
+          title="Change Password"
+         description="Enter your current password"
+         className="max-w-md w-full animate-fade-in-from-top "
+          content={<PasswordChangeForm />} />
+        
+      </div>
+      </>   
+        
     );
 };
 
