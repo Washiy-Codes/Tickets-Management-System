f14c46b7561607958e4aafeb03d4a6dc7d156b14
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-08 17:05:57 +0300

    Add Inngest API routes

---
+++ app/inngest/route.ts
@@ -0,0 +1,10 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest";
+import { passwordResetEvent } from "@/features/password/event/event-password-reset";
+
+
+export const {PUT, POST, GET} = serve({
+    client: inngest,
+    functions: [passwordResetEvent]
+
+})
