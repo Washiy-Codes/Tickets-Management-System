4c72d40ca88723db7171a1769b5118a1e546814b
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-13 23:54:12 +0300

    feat(org): add memberships admin page

--- app/(authenticated)/organization/[organizationId]/(admin)/memberships/page.tsx
+++ app/(authenticated)/organization/[organizationId]/(admin)/memberships/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { MembershipList } from "@/features/membership/components/membership-list";
 import Spinner from "@/components/spinner";
+import { OrganizationBreadcrumbs } from "../_navigation/tabs";
+import { InvitationCreateButton } from "@/features/invitation/components/invitation-create-btn";
 
 type MembershipsPageProps = {
   params: Promise<{
@@ -17,6 +19,8 @@ const MembershipsPage = async ({ params }: MembershipsPageProps) => {
       <Heading
         title="Memberships"
         description="Manage members in your organization"
+        tabs={<OrganizationBreadcrumbs />}
+        action={<InvitationCreateButton organizationId={organizationId} />}
       />
       <Suspense fallback={<Spinner />}>
         <MembershipList organizationId={organizationId} />
